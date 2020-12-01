08b52e017f1a96a1fb185c6389455884e58a8cf8
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2020-12-01 20:37:25 +0100

    Solved puzzle 1b (and added some stats)

--- puzzle1/solve.ts
+++ puzzle1/solve.ts
@@ -3,41 +3,78 @@ import path from 'path'
 
 type Result = {
   found: boolean,
-  firstIndex: number,
-  lastIndex: number,
+  indices: number[],
+  additions: number
 }
 
-function searchForValue(expenses: number[], value: number): Result {
-  const result = {
+function numericAscending(a: number, b: number): number {
+  return a - b
+}
+
+function readExpenses(fileWithPath: string): number[] {
+  return fs.readFileSync(fileWithPath)
+    .toString()
+    .split('\n')
+    .map(Number)
+    .sort(numericAscending)
+}
+
+function createResult(indices: number[]): Result {
+  return {
     found: false,
-    firstIndex: 0,
-    lastIndex: expenses.length - 1
-  } as Result
+    indices,
+    additions: 0
+  }
+}
+
+function printResult(result: Result): void {
+  console.log(result)
+  if (result.found) {
+    console.log(result.indices.map(index => expenses[index]).join(' + ') + ' = ' + result.indices.reduce((sum, index) => sum + expenses[index], 0))
+    console.log(result.indices.map(index => expenses[index]).join(' * ') + ' = ' + result.indices.reduce((product, index) => product * expenses[index], 1))
+  }
+}
+
+function searchTwo(expenses: number[], value: number): Result {
+  const result = createResult([0, expenses.length - 1])
 
   do {
-    const sum = expenses[result.firstIndex] + expenses[result.lastIndex]
+    result.additions++
+    const sum = expenses[result.indices[0]] + expenses[result.indices[1]]
     if (sum > value) {
-      result.lastIndex--
+      result.indices[1]--
     } else if (sum < value) {
-      result.firstIndex++
+      result.indices[0]++
     } else {
       result.found = true
-      return result
     }
-  } while (result.firstIndex < result.lastIndex)
+  } while (!result.found && result.indices[0] < result.indices[1])
 
   return result
 }
 
-const numericAscending = (a: number, b: number): number => a - b
-const fileWithPath = path.resolve(__dirname, 'expenses.csv')
-const expenses = fs.readFileSync(fileWithPath)
-  .toString()
-  .split('\n')
-  .map(Number)
-  .sort(numericAscending)
+function searchThree(expenses: number[], value: number): Result {
+  const result = createResult([0, 1, expenses.length - 1])
 
-const result = searchForValue(expenses, 2020)
-console.log(result)
-console.log(`${expenses[result.firstIndex]} + ${expenses[result.lastIndex]} = ${expenses[result.firstIndex] + expenses[result.lastIndex]}`)
-console.log(`${expenses[result.firstIndex]} * ${expenses[result.lastIndex]} = ${expenses[result.firstIndex] * expenses[result.lastIndex]}`)
+  do {
+    result.additions++
+    const sum = expenses[result.indices[0]] + expenses[result.indices[1]] + expenses[result.indices[2]]
+    if (sum > value) {
+      result.indices[2]--
+    } else if (sum < value) {
+      result.indices[1]++
+      if (result.indices[1] === result.indices[2]) {
+        result.indices[0]++
+        result.indices[1] = result.indices[0] + 1
+      }
+    } else if (sum === value) {
+      result.found = true
+    }
+  } while (!result.found)
+
+  return result
+}
+
+const expenses = readExpenses(path.resolve(__dirname, 'expenses.csv'))
+printResult(searchTwo(expenses, 2020))
+printResult(searchThree(expenses, 2020))
