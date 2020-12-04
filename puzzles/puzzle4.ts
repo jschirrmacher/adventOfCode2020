f945291502e2d24025ae0d167c8a26e8d3caa5ba
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2020-12-04 09:13:22 +0100

    Solved 4b

--- puzzles/puzzle4.ts
+++ puzzles/puzzle4.ts
@@ -28,15 +28,67 @@ function lineParser(line: string): Entry | null {
   }
 }
 
-function isValid(passport: Entry): boolean {
+function assert(condition: boolean) {
+  if (!condition) {
+    throw Error('doesn\t validate')
+  }
+}
+
+function validateYearRange(value: number, min: number, max: number): boolean {
+  try {
+    assert(value >= min)
+    assert(value <= max)
+    return true
+  } catch {
+    return false
+  }
+}
+
+function validateHeight(value: number, unit: string): boolean {
+  try {
+    if (unit === 'cm') {
+      assert(value >= 150 && value <= 193)
+    } else {
+      assert(value >= 59 && value <= 76)
+    }
+    return true
+  } catch {
+    return false
+  }
+}
+
+function isValid(passport: Entry, strict = false): boolean {
   const relevantFields = Object.entries(passport)
     .filter(entry => entry[0] !== 'cid')
-  return relevantFields.length === 7
+  
+  try {
+    assert(relevantFields.length === 7)
+    if (strict) {
+      assert(!!passport.byr.match(/^\d{4}$/) && validateYearRange(+passport.byr, 1920, 2002))
+      assert(!!passport.iyr.match(/^\d{4}$/) && validateYearRange(+passport.iyr, 2010, 2020))
+      assert(!!passport.eyr.match(/^\d{4}$/) && validateYearRange(+passport.eyr, 2020, 2030))
+      assert(!!passport.hgt.match(/^(1?\d\d)(in|cm)$/) && validateHeight(+RegExp.$1, RegExp.$2))
+      assert(!!passport.hcl.match(/^#[\da-f]{6}$/))
+      assert(!!passport.ecl.match(/^(amb|blu|brn|gry|grn|hzl|oth)$/))
+      assert(!!passport.pid.match(/^\d{9}$/))
+    }
+    return true
+  } catch (error) {
+    return false
+  }
 } 
 
 const passports = readInput(lineParser).concat(createEntryFromSavedLines()) as Entry[]
 
+function sortedFields(passport: Entry): string[][] {
+  return Object.keys(passport).sort().map(key => ([key, passport[key as keyof Entry]]))
+}
+
 export function run(): string {
-  // const debugOutput = passports.map(passport => `${Object.entries(passport).map(e => `${e[0]}: ${e[1]}`).join(' ')}: ${isValid(passport)}`).join('\n')
-  return 'Number of valid passports: ' + passports.filter(isValid).length
+  console.log(passports
+    .filter(passport => isValid(passport) && isValid(passport, true))
+    .map(passport => `${sortedFields(passport).map(e => `${e[0]}: ${e[1]}`).join(' ')}: ${isValid(passport, true)}`).join('\n')
+  )
+  return 'Number of valid passports with lax validation: ' + passports.filter(passport => isValid(passport)).length + 
+       '\nNumber of valid passports with strict validation: ' + passports.filter(passport => isValid(passport, true)).length
 }
