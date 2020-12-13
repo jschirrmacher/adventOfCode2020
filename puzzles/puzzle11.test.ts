51d6f8dc15f9d2f2a41bf706fc6b2f4f99b3f071
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2020-12-13 19:12:48 +0100

    Solved 11b

--- puzzles/puzzle11.test.ts
+++ puzzles/puzzle11.test.ts
@@ -1,5 +1,5 @@
 import 'should'
-import { run, solveA, doRound, addBorder, mapBinary } from './puzzle11'
+import { run, solveA, solveB, doRound, addBorder, mapBinary, occupiedSeatsAround } from './puzzle11'
 
 const testData = `L.LL.LL.LL
 LLLLLLL.LL
@@ -12,7 +12,11 @@ LLLLLLLLLL
 L.LLLLLL.L
 L.LLLLL.LL`.split('\n').map(mapBinary)
 
-const expectedAfterRounds = [
+function prepareExpected(expected: string[]): string[] {
+  return expected.map(s => addBorder(s.split('\n').map(r => mapBinary(r.trim()))).join(''))
+}
+
+const expectedAfterRounds = prepareExpected([
   `#.##.##.##
   #######.##
   #.#.#..#..
@@ -63,10 +67,73 @@ const expectedAfterRounds = [
   #L#L##L#L#
   #.LLLLLL.L
   #.#L#L#.##`
-].map(s => addBorder(s.split('\n').map(r => mapBinary(r.trim()))).join(''))
+])
+
+const expectedInPartB = prepareExpected([
+  `#.##.##.##
+  #######.##
+  #.#.#..#..
+  ####.##.##
+  #.##.##.##
+  #.#####.##
+  ..#.#.....
+  ##########
+  #.######.#
+  #.#####.##`,
+  `#.LL.LL.L#
+  #LLLLLL.LL
+  L.L.L..L..
+  LLLL.LL.LL
+  L.LL.LL.LL
+  L.LLLLL.LL
+  ..L.L.....
+  LLLLLLLLL#
+  #.LLLLLL.L
+  #.LLLLL.L#`,
+  `#.L#.##.L#
+  #L#####.LL
+  L.#.#..#..
+  ##L#.##.##
+  #.##.#L.##
+  #.#####.#L
+  ..#.#.....
+  LLL####LL#
+  #.L#####.L
+  #.L####.L#`,
+  `#.L#.L#.L#
+  #LLLLLL.LL
+  L.L.L..#..
+  ##LL.LL.L#
+  L.LL.LL.L#
+  #.LLLLL.LL
+  ..L.L.....
+  LLLLLLLLL#
+  #.LLLLL#.L
+  #.L#LL#.L#`,
+  `#.L#.L#.L#
+  #LLLLLL.LL
+  L.L.L..#..
+  ##L#.#L.L#
+  L.L#.#L.L#
+  #.L####.LL
+  ..#.#.....
+  LLL###LLL#
+  #.LLLLL#.L
+  #.L#LL#.L#`,
+  `#.L#.L#.L#
+  #LLLLLL.LL
+  L.L.L..#..
+  ##L#.#L.L#
+  L.L#.LL.L#
+  #.LLLL#.LL
+  ..#.L.....
+  LLL###LLL#
+  #.LLLLL#.L
+  #.L#LL#.L#`
+])
 
 describe('Puzzle 11', () => {
-  it('should match expected seat occupations', () => {
+  it('should match expected seat occupations for part A', () => {
     let seats = addBorder(testData).join('')
     expectedAfterRounds.forEach(expected => {
       const newSeats = doRound(seats, 12)
@@ -79,8 +146,55 @@ describe('Puzzle 11', () => {
     solveA(testData).should.equal(37)
   })
 
+  it('should see eight occupied seats for thw first example of part B', () => {
+    const seats = addBorder(`.......#.
+    ...#.....
+    .#.......
+    .........
+    ..#L....#
+    ....#....
+    .........
+    #........
+    ...#.....`.split('\n').map(r => mapBinary(r.trim()))).join('')
+
+    occupiedSeatsAround(seats, 11, true, seats.split('').findIndex(seat => seat === '0')).should.equal(8)
+  })
+
+  it('should see no occupied seats for the second example of part B', () => {
+    const seats = addBorder(`.............
+    .L.L.#.#.#.#.
+    .............`.split('\n').map(r => mapBinary(r.trim()))).join('')
+
+    occupiedSeatsAround(seats, 14, true, seats.split('').findIndex(seat => seat === '0')).should.equal(0)
+  })
+
+  it('should see no occupied seats for the third example of part B', () => {
+    const seats = addBorder(`.##.##.
+    #.#.#.#
+    ##...##
+    ...L...
+    ##...##
+    #.#.#.#
+    .##.##.`.split('\n').map(r => mapBinary(r.trim()))).join('')
+
+    occupiedSeatsAround(seats, 9, true, seats.split('').findIndex(seat => seat === '0')).should.equal(0)
+  })
+
+  it('should match expected seat occupations for part B', () => {
+    let seats = addBorder(testData).join('')
+    expectedInPartB.forEach((expected) => {
+      const newSeats = doRound(seats, 12, true)
+      newSeats.should.deepEqual(expected)
+      seats = newSeats
+    })
+  })
+
+  it('should solve part B', () => {
+    solveB(testData).should.equal(26)
+  })
+
   it('should output the result', () => {
-    run().should.match(/11a: \d+/)
+    run().should.match(/11a: \d+\n11b: \d+/)
   })
 })
 
--- puzzles/puzzle11.ts
+++ puzzles/puzzle11.ts
@@ -2,34 +2,42 @@ import readInput from "../lib/fileReader"
 
 export type Seats = string []
 
-export function doRound(seats: string, cols: number): string {
-  function occupiedSeatsAround(index: number): number {
-    return (+seats[index - cols - 1])
-         + (+seats[index - cols])
-         + (+seats[index - cols + 1])
-         + (+seats[index - 1])
-         + (+seats[index + 1])
-         + (+seats[index + cols - 1])
-         + (+seats[index + cols])
-         + (+seats[index + cols + 1])
+export function nextSeat(seats: string, cols: number, visibleSeats: boolean, index: number, offset: number): number {
+  const newIndex = index + offset
+  const newCol = newIndex % cols
+  if (newIndex < 0 || newIndex >= seats.length || newCol === 0 || newCol === cols - 1) {
+    return 0
   }
-  
+  const seat = seats[newIndex]
+  if (seat === ' ') {
+    return visibleSeats ? nextSeat(seats, cols, visibleSeats, newIndex, offset) : 0
+  } else {
+    return +seat
+  }
+}
+
+export function occupiedSeatsAround(seats: string, cols: number, visibleSeats: boolean, index: number): number {
+  const offsets = [ - cols - 1, - cols, - cols + 1, - 1, 1, cols - 1, cols, cols + 1] as number[]
+  return offsets.reduce((sum, offset) => sum + nextSeat(seats, cols, visibleSeats, index, offset), 0)
+}
+
+export function doRound(seats: string, cols: number, visibleSeats = false): string {
   return seats.replace(/[01]/g, (seat, index) => {
-    const occupied = occupiedSeatsAround(index)
+    const occupied = occupiedSeatsAround(seats, cols, visibleSeats, index)
     if (seat === '0' && occupied === 0) {
       return '1'
-    } else if (seat === '1' && occupied >= 4) {
+    } else if (seat === '1' && occupied >= (visibleSeats ? 5 : 4)) {
       return '0'
     }
     return seat
   })
 }
 
-function doRoundsUntilStable(seats: string, cols: number): string {
+function doRoundsUntilStable(seats: string, cols: number, visibleSeats = false): string {
   let newSeats: string = seats
   do {
     seats = newSeats
-    newSeats = doRound(seats, cols)
+    newSeats = doRound(seats, cols, visibleSeats)
   } while (seats !== newSeats)
   return newSeats
 }
@@ -45,6 +53,11 @@ export function solveA(input: string[]): number {
   return (seats.match(/1/g) || []).length
 }
 
+export function solveB(input: string[]): number {
+  const seats = doRoundsUntilStable(addBorder(input).join(''), input[0].length + 2, true)
+  return (seats.match(/1/g) || []).length
+}
+
 export function mapBinary(row: string): string {
   const mapping = {
     '#': '1',
@@ -56,5 +69,5 @@ export function mapBinary(row: string): string {
 
 export function run(): string {
   const input = readInput(line => mapBinary(line))
-  return '11a: ' + solveA(input)
+  return '11a: ' + solveA(input) + '\n11b: ' + solveB(input)
 }
