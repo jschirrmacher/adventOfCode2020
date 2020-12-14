ead9a2d36269ef3de01ac18abc23878db217d5a5
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2020-12-14 18:22:29 +0100

    Solved 13 b

--- puzzles/puzzle13.test.ts
+++ puzzles/puzzle13.test.ts
@@ -1,12 +1,21 @@
 import 'should'
-import { run, solveA } from './puzzle13'
+import { run, solveA, solveB } from './puzzle13'
 
 describe('Puzzle 13', () => {
   it('should solve part A', () => {
-    solveA(939, [ 7, 13, 59, 31, 19 ]).should.equal(295)
+    solveA(939, '7,13,x,x,59,x,31,19').should.equal(295)
+  })
+
+  it('should solve part B', () => {
+    solveB('7,13,x,x,59,x,31,19').should.equal(1068781)
+    solveB('17,x,13,19').should.equal(3417)
+    solveB('67,7,59,61').should.equal(754018)
+    solveB('67,x,7,59,61').should.equal(779210)
+    solveB('67,7,x,59,61').should.equal(1261476)
+    solveB('1789,37,47,1889').should.equal(1202161486)
   })
 
   it('should return the results', () => {
-    run().should.match(/13a: \d+/)
+    run().should.match(/13a: \d+\n13b: \d+/)
   })
 })
--- puzzles/puzzle13.ts
+++ puzzles/puzzle13.ts
@@ -1,10 +1,16 @@
 import readInput from "../lib/fileReader"
 
+type Bus = {
+  id: number,
+  offset: number
+}
+
 export function nextDepatureTime(time: number, id: number): number {
   return Math.ceil(time / id) * id
 }
 
-export function solveA(startTime: number, busIds: number[]): number {
+export function solveA(startTime: number, busIdString: string): number {
+  const busIds = busIdString.split(',').filter(id => id !== 'x').map(Number)
   const nextDepatureTimes = busIds.map(id => nextDepatureTime(startTime, id))
   const result = nextDepatureTimes.reduce((info, time, id) => {
     return time < info.time ? { id, time } : info
@@ -12,7 +18,30 @@ export function solveA(startTime: number, busIds: number[]): number {
   return (result.time - startTime) * busIds[result.id]
 }
 
+export function solveB(busIdString: string): number {
+  function getNextTimestamp(time: number, inc: number, offset: number, busId: number): number {
+    while ((time + offset) % busId !== 0) {
+      time += inc
+    }
+    return time
+  }
+
+  const buses: Bus[] = busIdString.split(',')
+    .map((id, offset) => ({ id: parseInt(id), offset }))
+    .filter(bus => !isNaN(bus.id))
+
+  let inc = 1
+  let time = 0
+  for (let index = 1; index < buses.length; index++) {
+    inc *= buses[index - 1].id
+    time = getNextTimestamp(time, inc, buses[index].offset, buses[index].id)
+  }
+
+  return time
+}
+
 export function run(): string {
   const input = readInput(line => line)
-  return '13a: ' + solveA(parseInt(input[0]), input[1].split(',').filter(id => id !== 'x').map(Number))
+  return '13a: ' + solveA(parseInt(input[0]), input[1])
+     + '\n13b: ' + solveB(input[1])
 }
