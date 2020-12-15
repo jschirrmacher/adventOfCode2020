73a67a5d891d60189576286635c20154765b1ba2
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2020-12-15 10:05:47 +0100

    Solved 14b

--- puzzles/puzzle14.test.ts
+++ puzzles/puzzle14.test.ts
@@ -1,17 +1,30 @@
 import 'should'
-import { run, solveA } from './puzzle14'
+import { applyMask, run, solveA, solveB } from './puzzle14'
 
 const testData = `mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
 mem[8] = 11
 mem[7] = 101
 mem[8] = 0`.split('\n')
 
+const testDataB = `mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1`.split('\n')
+
 describe('Puzzle 14', () => {
   it('should solve part A', () => {
     solveA(testData).should.equal(165)
   })
 
+  it('should apply an address mask', () => {
+    applyMask('101010', 'X1001X').should.equal('X1101X')
+  })
+
+  it('should solve part B', () => {
+    solveB(testDataB).should.equal(208)
+  })
+
   it('should return the results', () => {
-    run().should.match(/14a: \d+/)
+    run().should.match(/14a: \d+\n14b: \d+/)
   })
 })
--- puzzles/puzzle14.ts
+++ puzzles/puzzle14.ts
@@ -1,25 +1,55 @@
 import readInput from "../lib/fileReader"
 
-function createMachine() {
+type AssignmentFN = (address: number, value: bigint) => void
+
+export function applyMask(value: string, mask: string): string {
+  return value.split('').map((bit, pos) => {
+    if (mask[pos] === '1') {
+      return '1'
+    } else if (mask[pos] === 'X') {
+      return 'X'
+    } else {
+      return bit
+    }
+  }).join('')
+}
+
+function createMachine(type = 1) {
+  function assignValueToAddresses(adrMask: string, value: bigint): void {
+    if (adrMask.match(/X/)) {
+      assignValueToAddresses(adrMask.replace(/X/, '0'), value)
+      assignValueToAddresses(adrMask.replace(/X/, '1'), value)
+    } else {
+      machine.mem[parseInt(adrMask, 2)] = Number(value)
+    }
+  }
+
+  const assignments: Record<number, AssignmentFN> = {
+    1: (address: number, value: bigint) => {
+      machine.mem[address] = Number(value & machine.andMask | machine.orMask)
+    },
+
+    2: (address: number, value: bigint) => {
+      const binAdr = address.toString(2).padStart(36, '0')
+      const adrMask = applyMask(binAdr, machine.mask)
+      assignValueToAddresses(adrMask, value)
+    }
+  }
+
   const machine = {
+    mask: '0'.repeat(36),
     andMask: BigInt('0b' + '1'.repeat(36)),
-    orMask: BigInt('0b' + '0'.repeat(36)),
+    orMask: 0n,
     mem: {} as Record<number, number>,
 
     exec(command: string): void {
       if (command.match(/^mask = ([X01]{36})$/)) {
-        const mask = RegExp.$1
-        machine.andMask = BigInt('0b' + mask.replace(/X/g, '1'))
-        machine.orMask = BigInt('0b' + mask.replace(/X/g, '0'))
+        machine.mask = RegExp.$1
+        machine.andMask = BigInt('0b' + machine.mask.replace(/X/g, '1'))
+        machine.orMask = BigInt('0b' + machine.mask.replace(/X/g, '0'))
       } else {
-        const matches = command.match(/^mem\[(\d+)\] = (\d+)$/)
-        if (matches) {
-          const register = parseInt(matches[1])
-          const value = BigInt(matches[2]) & machine.andMask | machine.orMask
-          machine.mem[register] = Number(value)
-        } else {
-          console.error('Unknoen command ' + command)
-        }
+        const matches = command.match(/^mem\[(\d+)\] = (\d+)$/) as string[]
+        assignments[type](parseInt(matches[1]), BigInt(matches[2]))
       }
     },
 
@@ -37,7 +67,13 @@ export function solveA(input: string[]): number {
   return machine.getMemSum()
 }
 
+export function solveB(input: string[]): number {
+  const machine = createMachine(2)
+  input.forEach(machine.exec)
+  return machine.getMemSum()
+}
+
 export function run(): string {
   const input = readInput(line => line)
-  return '14a: ' + solveA(input)
+  return '14a: ' + solveA(input) + '\n14b: ' + solveB(input)
 }
