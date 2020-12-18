55f8577361679c1fce41e0653de5db0ef169d4f9
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2020-12-18 19:34:17 +0100

    Solved 17b

--- puzzles/puzzle17.test.ts
+++ puzzles/puzzle17.test.ts
@@ -1,5 +1,5 @@
 import 'should'
-import { run, doCycle, makeCubes, solveA, Cubes, createCoordinate, exists, addCoordinates, neighbors } from './puzzle17'
+import { run, doCycle, makeCubes, solveA, Cubes, createCoordinate, exists, addCoordinates, neighbors, solveB } from './puzzle17'
 
 const testData = `.#.
 ..#
@@ -88,34 +88,43 @@ const expectedAt3Cycles = `.......
 .......`
 
 function stringify(cubes: Cubes): string {
-  const min = (field: number): number => [...cubes].length && [...cubes].reduce((min, c) => Math.min(min, +c.split(' ')[field]), Number.MAX_SAFE_INTEGER)
-  const max = (field: number): number => [...cubes].length && [...cubes].reduce((max, c) => Math.max(max, +c.split(' ')[field]), Number.MIN_SAFE_INTEGER)
+  const min = (field: number): number => cubes.active.size && [...cubes.active].reduce((min, c) => Math.min(min, +c.split(' ')[field] || 0), Number.MAX_SAFE_INTEGER)
+  const max = (field: number): number => cubes.active.size && [...cubes.active].reduce((max, c) => Math.max(max, +c.split(' ')[field] || 0), Number.MIN_SAFE_INTEGER)
   const size = (field: number): number => max(field) - min(field) + 1
   const empty = (field: number, fill: unknown): unknown[] => {
-    const arr = Array(size(field)).fill(JSON.stringify(fill))
-    return JSON.parse('[' + arr.join(',') + ']')
+    try {
+      if (field >= cubes.dimensions) {
+        return [fill]
+      }
+      const arr = Array(size(field)).fill(JSON.stringify(fill))
+      return JSON.parse('[' + arr.join(',') + ']')
+    } catch (error) {
+      debugger
+      return []
+    }
   }
 
-  const matrix = empty(2, empty(1, empty(0, '.'))) as string[][][]
-  const origin = createCoordinate(-min(0), -min(1), -min(2))
-  cubes.forEach(cube => {
-    const [x, y, z] = addCoordinates(cube, origin).split(' ').map(Number)
+  const matrix = empty(3, empty(2, empty(1, empty(0, '.')))) as string[][][][]
+  const origin = createCoordinate(-min(0), -min(1), -min(2), -min(3))
+  cubes.active.forEach(cube => {
+    const [x, y, z, w] = addCoordinates(cube, origin).split(' ').map(Number)
     try {
-      matrix[z][y][x] = '#'
+      matrix[w][z][y][x] = '#'
     } catch (error) {
       debugger
     }
   })
-  return matrix.map(tier => tier.map(row => row.join('')).join('\n')).join('\n\n')
+  return matrix.map(space => space.map(tier => tier.map(row => row.join('')).join('\n')).join('\n\n')).join('\n\n')
 }
 
 describe('Puzzle 17', () => {
   it('should generate the data structure', () => {
-    stringify(makeCubes(testData)).should.equal(testData.join('\n'))
+    stringify(makeCubes(testData, 3)).should.equal(testData.join('\n'))
+    stringify(makeCubes(testData, 4)).should.equal(testData.join('\n'))
   })
 
   it('should identify existing cubes', () => {
-    const cubes = makeCubes(testData)
+    const cubes = makeCubes(testData, 3)
     exists(cubes, createCoordinate(-1, -1, 0)).should.be.false()
     exists(cubes, createCoordinate(0, 0, 0)).should.be.false()
     exists(cubes, createCoordinate(2, 1, 0)).should.be.true()
@@ -123,31 +132,38 @@ describe('Puzzle 17', () => {
   })
 
   it('should add coordinates', () => {
-    addCoordinates(createCoordinate(2, 1, 0), createCoordinate(-1, 2, 0)).should.deepEqual(createCoordinate(1, 3, 0))
+    addCoordinates(createCoordinate(2, 1, 0), createCoordinate(-1, 2, 0)).should.deepEqual(createCoordinate(1, 3, 0, 0))
+    addCoordinates(createCoordinate(2, 1, 0, -2), createCoordinate(-1, 2, 0, 1)).should.deepEqual(createCoordinate(1, 3, 0, -1))
   })
 
   it('should find neighbors', () => {
-    const cubes = makeCubes(testData)
+    const cubes = makeCubes(testData, 3)
     neighbors(cubes, createCoordinate(2, 1, 0)).should.deepEqual([createCoordinate(1, 0, 0), createCoordinate(1, 2, 0), createCoordinate(2, 2, 0)])
   })
 
   it('should do 1 cycle', () => {
-    stringify(doCycle(makeCubes(testData))).should.equal(expectedAt1Cycle)
+    stringify(doCycle(makeCubes(testData, 3))).should.equal(expectedAt1Cycle)
   })
 
   it('should do 2 cycles', () => {
-    stringify(doCycle(doCycle(makeCubes(testData)))).should.equal(expectedAt2Cycles)
+    stringify(doCycle(doCycle(makeCubes(testData, 3)))).should.equal(expectedAt2Cycles)
   })
 
   it('should do 3 cycles', () => {
-    stringify(doCycle(doCycle(doCycle(makeCubes(testData))))).should.equal(expectedAt3Cycles)
+    stringify(doCycle(doCycle(doCycle(makeCubes(testData, 3))))).should.equal(expectedAt3Cycles)
   })
 
   it('should solve part A', () => {
     solveA(testData).should.equal(112)
   })
 
-  it('should return the result', () => {
-    run().should.match(/17a: \d+/)
+  it('should solve part B', function () {
+    this.timeout(30000)
+    solveB(testData).should.equal(848)
+  })
+
+  it('should return the result', function () {
+    this.timeout(30000)
+    run().should.match(/17a: \d+\n17b: \d+/)
   })
 })
--- puzzles/puzzle17.ts
+++ puzzles/puzzle17.ts
@@ -2,56 +2,74 @@ import readInput from "../lib/fileReader"
 
 export type Coordinate = string
 
-export type Cubes = Set<string>
+export type Cubes = {
+  dimensions: number,
+  active: Set<string>,
+}
 
-export function createCoordinate(x: number, y: number, z: number): Coordinate {
-  return x + ' ' + y + ' ' + z
+export function createCoordinate(...parts: number[]): Coordinate {
+  return parts.join(' ') + (parts.length === 3 ? ' 0' : '')
 }
 
 const surrounding = [] as Coordinate[]
 [-1, 0, 1].forEach(x => {
   [-1, 0, 1].forEach(y => {
     [-1, 0, 1].forEach(z => {
-      if (x || y || z) {
-        surrounding.push(createCoordinate(x, y, z))
-      }
+      [-1, 0, 1].forEach(w => {
+        if (x || y || z || w) {
+          surrounding.push(createCoordinate(x, y, z, w))
+        }
+      })
     })
   })
 })
 
+function dimension(dim: number) {
+  if (dim === 4) {
+    return (coord: Coordinate) => coord
+  }
+  return (coord: Coordinate) => coord.split(' ')[3] === '0'
+}
+
 export function addCoordinates(coord1: Coordinate, coord2: Coordinate): Coordinate {
-  const [ x1, y1, z1 ] = coord1.split(' ').map(Number)
-  const [ x2, y2, z2 ] = coord2.split(' ').map(Number)
-  return createCoordinate(x1 + x2, y1 + y2, z1 + z2)
+  const one = coord1.split(' ').map(Number)
+  const two = coord2.split(' ').map(Number)
+  return createCoordinate(...one.map((p, i) => p + two[i]))
 }
 
 export function exists(cubes: Cubes, coordinate: Coordinate): boolean {
-  return cubes.has(coordinate)
+  return cubes.active.has(coordinate)
 }
 
 export function neighbors(cubes: Cubes, cube: Coordinate): Coordinate[] {
-  return surrounding
+  return surrounding.filter(dimension(cubes.dimensions))
     .map(c => addCoordinates(c, cube))
     .filter(c => exists(cubes, c))
 }
 
 export function doCycle(cubes: Cubes): Cubes {
   const newActive = [] as Coordinate[]
-  const staysActive = [...cubes].filter(cube => {
-    const directNeighbors = surrounding.map(c => addCoordinates(c, cube))
+  const staysActive = [...cubes.active].filter(cube => {
+    const directNeighbors = surrounding.filter(dimension(cubes.dimensions)).map(c => addCoordinates(c, cube))
     directNeighbors.filter(c => neighbors(cubes, c).length === 3).forEach(c => newActive.push(c))
     return [2, 3].includes(directNeighbors.filter(c => exists(cubes, c)).length)
   })
-  return new Set([ ...staysActive, ...newActive ])
+  return {
+    dimensions: cubes.dimensions,
+    active: new Set([ ...staysActive, ...newActive ])
+  }
 }
 
-export function makeCubes(startFlatRegion: string[]): Cubes {
-  const cubes = new Set() as Cubes
+export function makeCubes(startFlatRegion: string[], dimensions: number): Cubes {
+  const cubes = {
+    dimensions,
+    active: new Set()
+  } as Cubes
 
   startFlatRegion.forEach((row, y) => {
     row.split('').forEach((cube, x) => {
       if (cube === '#') {
-        cubes.add(createCoordinate(x, y, 0))
+        cubes.active.add(createCoordinate(x, y, 0))
       }
     })
   })
@@ -60,14 +78,22 @@ export function makeCubes(startFlatRegion: string[]): Cubes {
 }
 
 export function solveA(startFlatRegion: string[]): number {
-  let cubes = makeCubes(startFlatRegion)
+  let cubes = makeCubes(startFlatRegion, 3)
   for (let i=0; i < 6; i++) {
     cubes = doCycle(cubes)
   }
-  return [...cubes].length
+  return cubes.active.size
+}
+
+export function solveB(startFlatRegion: string[]): number {
+  let cubes = makeCubes(startFlatRegion, 4)
+  for (let i=0; i < 6; i++) {
+    cubes = doCycle(cubes)
+  }
+  return cubes.active.size
 }
 
 export function run(): string {
   const input = readInput(line => line)
-  return '17a: ' + solveA(input)
+  return '17a: ' + solveA(input) + '\n17b: ' + solveB(input)
 }
