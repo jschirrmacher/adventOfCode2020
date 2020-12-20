c88ac01bb78a91aff7208069d7d4d56c9d50c26d
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2020-12-20 17:08:58 +0100

    Parse into functions

--- puzzles/puzzle19.test.ts
+++ puzzles/puzzle19.test.ts
@@ -1,5 +1,5 @@
 import 'should'
-import { parse, buildTree, solveA, run } from './puzzle19'
+import { parse, parseB, solve, run } from './puzzle19'
 
 const testData = `0: 4 1 5
 1: 2 3 | 3 2
@@ -14,31 +14,106 @@ abbbab
 aaabbb
 aaaabbb`
 
+const testDataB = `42: 9 14 | 10 1
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+1: "a"
+11: 42 31
+5: 1 14 | 15 1
+19: 14 1 | 14 14
+12: 24 14 | 19 1
+16: 15 1 | 14 14
+31: 14 17 | 1 13
+6: 14 14 | 1 14
+2: 1 24 | 14 4
+0: 8 11
+13: 14 3 | 1 12
+15: 1 | 14
+17: 14 2 | 1 7
+23: 25 1 | 22 14
+28: 16 1
+4: 1 1
+20: 14 14 | 1 15
+3: 5 14 | 16 1
+27: 1 6 | 14 18
+14: "b"
+21: 14 1 | 1 14
+25: 1 1 | 1 14
+22: 14 14
+8: 42
+26: 14 22 | 1 20
+18: 15 15
+7: 14 5 | 1 21
+24: 14 1
+
+abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+bbabbbbaabaabba
+babbbbaabbbbbabbbbbbaabaaabaaa
+aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+bbbbbbbaaaabbbbaaabbabaaa
+bbbababbbbaaaaaaaabbababaaababaabab
+ababaaaaaabaaab
+ababaaaaabbbaba
+baabbaaaabbaaaababbaababb
+abbbbabbbbaaaababbbbbbaaaababb
+aaaaabbaabaaaaababaa
+aaaabbaaaabbaaa
+aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+babaaabbbaaabaababbaabababaaab
+aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba`
+
 const parsed = parse(testData.split('\n'))
-const tree = buildTree(parsed.rules)
+const tree = parsed.rules[0]
+const parsedB = parseB(testDataB.split('\n'))
+const treeB = parsed.rules[0]
 
 describe('Puzzle 19', () => {
-  it('should parse rules', () => {
-    parsed.rules.should.deepEqual(['4 1 5', '2 3 | 3 2', '4 4 | 5 5', '4 5 | 5 4', '"a"', '"b"'])
-  })
-
   it('should parse messages', () => {
     parsed.messages.should.deepEqual(['ababbb', 'bababa', 'abbbab', 'aaabbb', 'aaaabbb'])
   })
 
   it('should validate test data entries', () => {
-    tree('ababbb').should.equal(6)
-    tree('abbbab').should.equal(6)
-    tree('bababa').should.not.equal(6)
-    tree('aaabbb').should.not.equal(6)
-    tree('aaaabbb').should.not.equal(7)
+    tree('ababbb').matched.should.be.true()
+    tree('abbbab').matched.should.be.true()
+    tree('bababa').matched.should.be.false()
+    tree('aaabbb').matched.should.be.false()
+    tree('aaaabbb').matched.should.be.false()
   })
 
   it('should solve part A', () => {
-    solveA(parsed).should.equal(2)
+    solve(parsed).should.equal(2)
+  })
+
+  it('should reject "aaaabbaaaabbaaa" with modified ruleset', () => {
+    treeB('aaaabbaaaabbaaa').matched.should.be.false()
+  })
+
+  it('should validate test data for part B', () => { 
+    const result = Object.assign({}, ...parsedB.messages.map(message => ({ [message]: treeB(message).matched })))
+    result.should.equal({
+      abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa: false,
+      bbabbbbaabaabba: true,
+      babbbbaabbbbbabbbbbbaabaaabaaa: true,
+      aaabbbbbbaaaabaababaabababbabaaabbababababaaa: true,
+      bbbbbbbaaaabbbbaaabbabaaa: true,
+      bbbababbbbaaaaaaaabbababaaababaabab: true,
+      ababaaaaaabaaab: true,
+      ababaaaaabbbaba: true,
+      baabbaaaabbaaaababbaababb: true,
+      abbbbabbbbaaaababbbbbbaaaababb: true,
+      aaaaabbaabaaaaababaa: true,
+      aaaabbaaaabbaaa: false,
+      aaaabbaabbaaaaaaabbbabbbaaabbaabaaa: true,
+      babaaabbbaaabaababbaabababaaab: false,
+      aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba: true,
+    })
+})
+
+  it('should solve part B', () => {
+    solve(parsedB).should.equal(12)
   })
 
   it('should return the result', () => {
-    run().should.match(/^19a: \d+/)
+    run().should.match(/^19a: \d+\n19b: \d+x/)
   })
 })
--- puzzles/puzzle19.ts
+++ puzzles/puzzle19.ts
@@ -1,19 +1,54 @@
 import readInput from "../lib/fileReader"
 
-export type Rule = string
+export type Rule = Matcher
 
 export type Input = {
   rules: Rule[]
   messages: string[]
 }
 
+type MatcherResult = { matched: boolean, len: number }
+type Matcher = (message: string) => MatcherResult
+
 export function parse(input: string[]): Input {
-  const rules = [] as string[]
+  const rules = [] as Matcher[]
   const messages = [] as string[]
+  const cache = {} as Record<string, MatcherResult>
+
+  function parseRule(rule: string): Matcher {
+    const matches = rule.match(/^"(.)"$/)
+    if (matches) {
+      return message => {
+        return { matched: message.startsWith(matches[1]), len: 1 }
+      }
+    }
+    const variants = rule.split('|').map(part => {
+      return part.trim()
+        .split(' ')
+        .map(Number)
+    })
+  
+    return message => {
+      function listMatches(list: number[]): MatcherResult {
+        return list.reduce((prev: MatcherResult, ruleId: number) => {
+          return rules[ruleId](message.substr(prev.len))
+        }, { matched: false, len: 0 })
+      }
+  
+      for (const list of variants) {
+        const result = listMatches(list)
+        if (result.matched) {
+          return result
+        }
+      }
+      return { matched: false, len: 0 }
+    }
+  }
+    
   input.forEach(line => {
     const matches = line.match(/^(\d+):\s*(.*)$/)
     if (matches) {
-      rules[+matches[1]] = matches[2]
+      rules[+matches[1]] = parseRule(matches[2])
     } else if (line.trim() !== '') {
       messages.push(line)
     }
@@ -21,31 +56,20 @@ export function parse(input: string[]): Input {
   return { rules, messages }
 }
 
-export function buildTree(rules: Rule[], ruleNo = 0): (message: string) => number {
-  function handleList(listStr: string): (message: string) => number {
-    return message => listStr.trim().split(' ').reduce((pos, ruleNo) => {
-      return pos + buildTree(rules, +ruleNo)(message.substr(pos))
-    }, 0)
-  }
-
-  const rule = rules[ruleNo]
-  const matches = rule.match(/^"(.)"$/)
-  if (matches) {
-    return message => message.startsWith(matches[1]) ? 1 : 0
-  }
-  const variants = rule.split('|')
-  return message => {
-    return variants.map(variant => handleList(variant)(message))
-      .reduce((max, current) => Math.max(max, current))
-  }
+export function solve(input: Input): number {
+  return input.messages.filter(message => input.rules[0](message).matched).length
 }
 
-export function solveA(input: Input): number {
-  const tree = buildTree(input.rules)
-  return input.messages.filter(message => tree(message) === message.length).length
+export function parseB(input: string[]): Input {
+  const modified = input
+    .join('\n')
+    .replace(/8: .*/, '8: 42 | 42 8')
+    .replace(/11: .*/, '11: 42 31 | 42 11 31')
+    .split('\n')
+  return parse(modified)
 }
 
 export function run(): string {
-  const input = parse(readInput(line => line))
-  return '19a: ' + solveA(input)
+  const input = readInput(line => line)
+  return '19a: ' + solve(parse(input)) + '\n19b: ' + solve(parseB(input))
 }
